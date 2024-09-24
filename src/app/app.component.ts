48b7223e7c573329d6025ee3b1623cb58a8e818c
OTMJ <elmejbarotmane@gmail.com>
2024-09-24 14:57:06 +0100

    navbar + first product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListProduitsComponent } from "./list-produits/list-produits.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListProduitsComponent],
+  imports: [RouterOutlet, ListProduitsComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ecom-app';
-  filliere : string =  'Genie informatique';
 }
