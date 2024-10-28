5206c10a079ce6b74c57c4d42c2ac19bce298816
med03ali <medali24122003@gmail.com>
2024-10-28 10:19:17 +0100

    prepare for heroku

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
+import { AuthGuardService } from './auth-guard.service';
 import { Routes } from '@angular/router';
 import {ListProduitsComponent} from "./list-produits/list-produits.component";
 import {PanierComponent} from "./panier/panier.component";
 import {DetailsProduitsComponent} from "./details-produit/details-produit.component";
-import {AuthGuardService} from "./auth-guard.service";
 import {AuthentificationComponent} from "./authentification/authentification.component";
 import {UserProfileComponent} from "./user-profile/user-profile.component";
 import { SignupComponent } from './signup/signup.component';
